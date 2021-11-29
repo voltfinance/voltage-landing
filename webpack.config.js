da6423833c5e1af9d2080f797edf437b81b45989
Lior Agnin <lior.agnin@gmail.com>
2021-11-29 14:48:22 +0200

    m

--- webpack.config.js
+++ webpack.config.js
@@ -146,19 +146,6 @@ module.exports = {
           }
         ]
       },
-      // {
-      //   test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //       options: {
-      //         name: '[name].[ext]',
-      //         outputPath: 'fonts',
-      //         publicPath: './fonts'
-      //       }
-      //     }
-      //   ]
-      // }
     ]
   },
   resolve: {
