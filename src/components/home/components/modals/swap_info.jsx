ecde8c16a7e66bdc31920d0fb8b96f899cd6259b
Lior Agnin <lior.agnin@gmail.com>
2022-03-06 14:40:01 +0200

    CSS - move inline to classess

--- src/components/home/components/modals/swap_info.jsx
+++ src/components/home/components/modals/swap_info.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import voltInfo from '@/assets/images/volt_info.png'
 import docs from '@/assets/images/voltage_docs.svg'
 
-function SwapInfoModal() {
+function SwapInfoModal () {
   return (
     <div className='swap_info_modal'>
-      <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '850px' }}>
-        <p style={{ textAlign: 'center', fontSize: '18px', paddingBottom: '8px' }}>The Fuse Ecosystem Round is focused on offering the community the control of Voltage.</p>
-        <p style={{ textAlign: 'center', fontSize: '18px', paddingBottom: '15px' }}>The Volt tokens will have voting power and much more</p>
-        <img src={voltInfo} style={{margin: 'auto'}} width='685px' />
-
-        <a rel='noreferrer noopener' target='_blank' href='https://docs.voltage.finance' className='link' style={{marginTop: '10px'}}>
+      <div className='swap_info_modal__container'>
+        <p className='swap_info_modal__title'>The Fuse Ecosystem Round is focused on offering the community the control of Voltage.</p>
+        <p className='swap_info_modal__subtitle'>The Volt tokens will have voting power and much more</p>
+        <img className='swap_info_modal__info_img' src={voltInfo} />
+        <a rel='noreferrer noopener' target='_blank' href='https://docs.voltage.finance' className='link'>
           {' '}
           <img src={docs} />
           Voltage Docs →
